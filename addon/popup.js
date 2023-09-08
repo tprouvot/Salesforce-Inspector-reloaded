80bb4018878f707eef65975c9502b8f6ae663b5b
Thomas Prouvot <35368290+tprouvot@users.noreply.github.com>
2023-09-08 15:06:42 +0200

    Fix/object explorer fix (#151)
    Fix #138

--- addon/popup.js
+++ addon/popup.js
@@ -385,21 +385,31 @@ class AllDataBox extends React.PureComponent {
       });
     }
 
-    function getEntityDefinitions(bucket) {
-      let query = "select QualifiedApiName, Label, KeyPrefix, DurableId, IsCustomSetting from EntityDefinition" + bucket;
-      return sfConn.rest("/services/data/v" + apiVersion + "/tooling/query?q=" + encodeURIComponent(query)).then(res => {
-        for (let record of res.records) {
-          addEntity({
-            name: record.QualifiedApiName,
-            label: record.Label,
-            keyPrefix: record.KeyPrefix,
-            durableId: record.DurableId,
-            isCustomSetting: record.IsCustomSetting
-          }, null);
-        }
-      }).catch(err => {
-        console.error("list entity definitions: " + bucket, err);
-      });
+    function getEntityDefinitions(){
+      return sfConn.rest("/services/data/v" + apiVersion + "/tooling/query?q=" + encodeURIComponent("SELECT COUNT() FROM EntityDefinition"))
+        .then(res => {
+          let entityNb = res.totalSize;
+          for (let bucket = 0; bucket < Math.ceil(entityNb / 2000); bucket++) {
+            let offset = bucket > 0 ? " OFFSET " + (bucket * 2000) : "";
+            let query = "SELECT QualifiedApiName, Label, KeyPrefix, DurableId, IsCustomSetting FROM EntityDefinition ORDER BY QualifiedApiName ASC LIMIT 2000" + offset;
+            sfConn.rest("/services/data/v" + apiVersion + "/tooling/query?q=" + encodeURIComponent(query))
+              .then(respEntity => {
+                for (let record of respEntity.records) {
+                  addEntity({
+                    name: record.QualifiedApiName,
+                    label: record.Label,
+                    keyPrefix: record.KeyPrefix,
+                    durableId: record.DurableId,
+                    isCustomSetting: record.IsCustomSetting
+                  }, null);
+                }
+              }).catch(err => {
+                console.error("list entity definitions: ", err);
+              });
+          }
+        }).catch(err => {
+          console.error("count entity definitions: ", err);
+        });
     }
 
     Promise.all([
@@ -411,11 +421,8 @@ class AllDataBox extends React.PureComponent {
       // These records are less interesting than the ones the user has access to, but still interesting since we can get information about them using the tooling API
       // If there are too many records, we get "EXCEEDED_ID_LIMIT: EntityDefinition does not support queryMore(), use LIMIT to restrict the results to a single batch"
       // We cannot use limit and offset to work around it, since EntityDefinition does not support those according to the documentation, and they seem to work in a querky way in practice.
-      // Tried to use http://salesforce.stackexchange.com/a/22643, but "order by x" uses AaBbCc as sort order, while "where x > ..." uses sort order ABCabc, so it does not work on text fields, and there is no unique numerical field we can sort by.
-      // Here we split the query into a somewhat arbitrary set of fixed buckets, and hope none of the buckets exceed 2000 records.
-      getEntityDefinitions(" WHERE QualifiedApiName < 'M' LIMIT 2000"),
-      getEntityDefinitions(" WHERE QualifiedApiName >= 'M' AND QualifiedApiName < 'U' LIMIT 2000"),
-      getEntityDefinitions(" WHERE QualifiedApiName >= 'U' LIMIT 2000"),
+      // Even if documentation mention that LIMIT and OFFSET are not supported, we use it to split the EntityDefinition queries into 2000 buckets
+      getEntityDefinitions(),
     ])
       .then(() => {
         // TODO progressively display data as each of the three responses becomes available

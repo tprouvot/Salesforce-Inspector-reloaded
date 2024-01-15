5197d6825c5057eeaaa4d9a060b33668949f3b47
Thomas Prouvot <prouvot.t@gmail.com>
2024-01-15 16:44:06 +0100

    Use variable for className

--- addon/button.js
+++ addon/button.js
@@ -151,13 +151,14 @@ function initButton(sfHost, inInspector) {
         showStdPageDetails(e.data.insextData, e.data.insextAllFieldSetupLinks);
       }
       if (e.data.insextShowApiName) {
+        let apiNamesClass = "field-api-name";
         if (e.data.btnLabel.startsWith("Show")){
           document.querySelectorAll("record_flexipage-record-field > div, records-record-layout-item > div, div .forcePageBlockItemView").forEach(field => {
             let label = field.querySelector("span");
             if (field.dataset.targetSelectionName && label.querySelector("mark") == null){
               label.innerText = label.innerText + " ";
               const fieldApiName = document.createElement("mark");
-              fieldApiName.className = "field-api-name";
+              fieldApiName.className = apiNamesClass;
               fieldApiName.style.cursor = "copy";
               fieldApiName.innerText = field.dataset.targetSelectionName.split(".")[2];
               label.appendChild(fieldApiName);
@@ -165,7 +166,7 @@ function initButton(sfHost, inInspector) {
             }
           });
         } else {
-          document.querySelectorAll(".field-api-name").forEach(e => e.remove());
+          document.querySelectorAll(apiNamesClass).forEach(e => e.remove());
         }
       }
     });

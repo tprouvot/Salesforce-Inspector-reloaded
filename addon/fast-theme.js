2f67810f0be5f4afa6c4a4b0c404248fce460232
Astisme <alum.banyans0w@icloud.com>
2024-02-10 08:41:41 +0100

    correct theme on page load and reload with fast-theme.js

---
+++ addon/fast-theme.js
@@ -0,0 +1,11 @@
+console.log("running");
+const html = document.documentElement;
+let savedTheme = localStorage.getItem("preferredColorScheme");
+if (savedTheme == null){
+  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+  savedTheme = prefersDarkScheme.matches ? "dark" : "light";
+}
+html.dataset.theme = savedTheme;
+const savedAccent = localStorage.getItem("preferredAccentScheme");
+html.dataset.accent = savedAccent;
+console.log("done");

18352e093bf3241be5169efdc843a2245172e020
olivier Dufour <olivier.duff@gmail.com>
2024-05-29 16:21:09 +0200

    fix chrome and firefox build

--- scripts/dev-build.js
+++ scripts/dev-build.js
@@ -13,14 +13,21 @@ if (browser == "firefox") {
   // Firefox needs to run in spanning mode, since it does not support split mode.
   manifest.incognito = "spanning";
 
+  manifest.permissions = manifest.permissions.concat(manifest["host_permissions"]);
+  manifest["web_accessible_resources"] = manifest["web_accessible_resources"][0].resources;
+  delete manifest["host_permissions"];
   // Remove unused property, for consistency with the Chrome version.
   delete manifest.minimum_chrome_version;
+  delete manifest["version_name"];
 
 } else if (browser == "chrome") {
 
   // Chrome needs to run in split mode, since it does not support opening private extension tabs in spanning mode.
   manifest.incognito = "split";
-
+  manifest.background["service_worker"] = manifest.background.scripts[0];
+  delete manifest.background.scripts;
+  manifest.background.type = "module";
+  manifest["manifest_version"] = 3;
   // Remove irrelevant but annoying warning message "Unrecognized manifest key 'applications'.".
   delete manifest.applications;
 

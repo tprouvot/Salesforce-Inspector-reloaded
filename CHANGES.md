73097e66d690b495abaa6930109b7ade62c4bf78
Thomas Prouvot <prouvot.t@gmail.com>
2024-08-21 15:37:58 +0200

    Enable reloaded on experience builder site

--- CHANGES.md
+++ CHANGES.md
@@ -2,6 +2,7 @@
 
 ## Version 1.25
 
+- Enable Salesforce Inspector Reloaded on Experience Builder
 - Ad option to colorize sandbox banner in the same color as the favicon [doc](https://tprouvot.github.io/Salesforce-Inspector-reloaded/how-to/?h=favico#customize-sandbox-banner-color)
 - Add "Object Access" link in Object popup tab to display Object permission details (Winter 25 feature)
 - Fix Platform Event links on popup [issue 500](https://github.com/tprouvot/Salesforce-Inspector-reloaded/issues/500)
--- addon/button.js
+++ addon/button.js
@@ -5,7 +5,7 @@
 // sfdcBody = normal Salesforce page
 // ApexCSIPage = Developer Console
 // auraLoadingBox = Lightning / Salesforce1
-if (document.querySelector("body.sfdcBody, body.ApexCSIPage, #auraLoadingBox") || location.host.endsWith("visualforce.com")) {
+if (document.querySelector("body.sfdcBody, body.ApexCSIPage, #auraLoadingBox, #studioBody") || location.host.endsWith("visualforce.com")) {
   // We are in a Salesforce org
   chrome.runtime.sendMessage({message: "getSfHost", url: location.href}, sfHost => {
     if (sfHost) {

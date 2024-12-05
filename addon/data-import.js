1725644e5c3b3335b9f2af6eeea21ca2a452e289
Alfredo <a.chissotti@reply.it>
2024-12-05 22:07:50 +0100

    updates to data-import

--- addon/data-import.js
+++ addon/data-import.js
@@ -1195,18 +1195,7 @@ class App extends React.Component {
                 )
               ),
               h("a", {className: "button field-info", href: model.showDescribeUrl(), target: "_blank", title: "Show field info for the selected object"},
-                h("div", {className: "button-icon"},
-                  h("svg", {className: "slds-icon", viewBox: "0 0 24 24"},
-                    h("path", {
-                      d: `
-                      M11 9c-.5 0-1-.5-1-1s.5-1 1-1 1 .5 1 1-.5 1-1 1z
-                      m1 5.8c0 .2-.1.3-.3.3h-1.4c-.2 0-.3-.1-.3-.3v-4.6c0-.2.1-.3.3-.3h1.4c.2.0.3.1.3.3z
-                      M11 3.8c-4 0-7.2 3.2-7.2 7.2s3.2 7.2 7.2 7.2s7.2-3.2 7.2-7.2s-3.2-7.2-7.2-7.2z
-                      m0 12.5c-2.9 0-5.3-2.4-5.3-5.3s2.4-5.3 5.3-5.3s5.3 2.4 5.3 5.3-2.4 5.3-5.3 5.3z
-                      M 17.6 15.9c-.2-.2-.3-.2-.5 0l-1.4 1.4c-.2.2-.2.3 0 .5l4 4c.2.2.3.2.5 0l1.4-1.4c.2-.2.2-.3 0-.5z
-                      `})
-                  )
-                )
+                h("div", {className: "button-icon"})
               )
             ),
             h("div", {className: "conf-line radio-buttons"},

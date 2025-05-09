eba22bbbacb28beeda8ca20a3382689c1c589f21
Thomas Prouvot <prouvot.t@gmail.com>
2025-05-09 10:38:40 +0200

    externalize global methods

--- addon/utils.js
+++ addon/utils.js
@@ -19,3 +19,16 @@ export function displayButton(buttonName, hideButtonsOption){
   //if no option was found, display the button
   return true;
 }
+
+export function getLatestApiVersionFromOrg(sfHost) {
+  let latestApiVersionFromOrg = sessionStorage.getItem(sfHost + "_latestApiVersionFromOrg");
+  if (latestApiVersionFromOrg != null) {
+    return latestApiVersionFromOrg;
+  } else {
+    sfConn.rest("services/data/").then(res => {
+      latestApiVersionFromOrg = res[res.length - 1].version; //Extract the value of the last version
+      sessionStorage.setItem(sfHost + "_latestApiVersionFromOrg", latestApiVersionFromOrg);
+      return latestApiVersionFromOrg;
+    });
+  }
+}

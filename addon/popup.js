107a55aa282b4302f6ae944d3f0831ae482910c8
Georgi Dobrishinov <dobrishinov@gmail.com>
2025-10-29 19:09:32 +0200

    Added missing fieldCreatorBtn css class to fix the next button for the Dependencies Explorer

--- addon/popup.js
+++ addon/popup.js
@@ -340,7 +340,7 @@ class App extends React.PureComponent {
               h("a", {ref: "limitsBtn", href: limitsHref, target: linkTarget, className: "page-button slds-button slds-button_neutral"}, h("span", {}, "Org ", h("u", {}, "L"), "imits"))
             ) : null,
             h("div", {className: "slds-m-bottom_xx-small"},
-              h("a", {ref: "fieldCreatorBtn", href: fieldCreatorHref, target: linkTarget, className: "page-button slds-button slds-button_neutral"}, h("span", {}, "Field Crea", h("u", {}, "t"), "or (beta)"))
+              h("a", {ref: "fieldCreatorBtn", href: fieldCreatorHref, target: linkTarget, className: "page-button slds-button slds-button_neutral"}, h("span", {}, "Field Crea", h("u", {}, "t"), "or"))
             ),
             h("div", {className: "slds-m-bottom_xx-small"},
               h("a", {ref: "dependenciesExplorerBtn", href: "dependencies-explorer.html?" + hostArg, target: linkTarget, className: "page-button slds-button slds-button_neutral"}, h("span", {}, "Dependencies Explorer"))

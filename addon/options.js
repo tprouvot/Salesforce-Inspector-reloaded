42625e0ae6c4d96079b11e00199a4c015678c53f
Victor Garcia Zarco <victor.gzarco@gmail.com>
2025-11-03 21:47:09 +0100

    fix: padding of buttons in options page

--- addon/options.js
+++ addon/options.js
@@ -1891,7 +1891,7 @@ class App extends React.Component {
     // Define utility items for this page (injected as "slots")
     const utilityItems = [
       // Export Options button
-      h("div", {className: "slds-builder-header__utilities-item slds-p-top_x-small"},
+      h("div", {className: "slds-builder-header__utilities-item slds-p-top_x-small slds-p-horizontal_x-small"},
         h("button", {
           className: "slds-button slds-button_icon slds-button_icon-border-filled",
           onClick: this.exportOptions,
@@ -1903,7 +1903,7 @@ class App extends React.Component {
         )
       ),
       // Import Options button
-      h("div", {className: "slds-builder-header__utilities-item slds-p-top_x-small"},
+      h("div", {className: "slds-builder-header__utilities-item slds-p-top_x-small slds-p-horizontal_x-small"},
         h("button", {
           className: "slds-button slds-button_icon slds-button_icon-border-filled",
           onClick: () => this.refs.fileInput.click(),

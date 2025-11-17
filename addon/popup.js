5e0e90efab575ec81767eaa8b2ebccb8b023b458
Thomas Prouvot <35368290+tprouvot@users.noreply.github.com>
2025-11-17 14:31:22 +0100

    Fix [bug]: Inspector popup is blank when setting the option "Default tab" to Org (#991)
    ## Describe your changes
    Fix Popup issue when Org is default tab
    
    ## Issue ticket number and link
    #988
    
    ## Checklist before requesting a review
    - [ ] I have read and understand the [Contributions
    section](https://github.com/tprouvot/Salesforce-Inspector-reloaded#contributions)
    - [ ] Target branch is releaseCandidate and not master
    - [ ] I have performed a self-review of my code
    - [ ] I ran the [unit
    tests](https://github.com/tprouvot/Salesforce-Inspector-reloaded#unit-tests)
    and my PR does not break any tests
    - [ ] I documented the changes I've made on the
    [CHANGES.md](https://github.com/tprouvot/Salesforce-Inspector-reloaded/blob/master/CHANGES.md)
    and followed actual conventions
    - [ ] I added a new section on
    [how-to.md](https://github.com/tprouvot/Salesforce-Inspector-reloaded/blob/master/docs/how-to.md)
    (optional)

--- addon/popup.js
+++ addon/popup.js
@@ -1,6 +1,6 @@
 /* global React ReactDOM */
 import {sfConn, apiVersion, sessionError} from "./inspector.js";
-import {getLinkTarget, displayButton, getLatestApiVersionFromOrg} from "./utils.js";
+import {getLinkTarget, displayButton, getLatestApiVersionFromOrg, setOrgInfo} from "./utils.js";
 import {getAllFieldSetupLinks} from "./setup-links.js";
 import {setupLinks} from "./links.js";
 import AlertBanner from "./components/AlertBanner.js";
@@ -246,24 +246,14 @@ class App extends React.PureComponent {
     }
   }
   componentDidMount() {
-    let {sfHost} = this.props;
     addEventListener("message", this.onContextUrlMessage);
     addEventListener("keydown", this.onShortcutKey);
     parent.postMessage({insextLoaded: true}, "*");
-    this.setOrgInfo(sfHost);
+    setOrgInfo(this.props.sfHost);
   }
   componentWillUnmount() {
     removeEventListener("message", this.onContextUrlMessage);
     removeEventListener("keydown", this.onShortcutKey);
-  }
-  setOrgInfo(sfHost) {
-    let orgInfo = JSON.parse(sessionStorage.getItem(sfHost + "_orgInfo"));
-    if (orgInfo == null) {
-      sfConn.rest("/services/data/v" + apiVersion + "/query/?q=SELECT+Id,InstanceName,OrganizationType+FROM+Organization").then(res => {
-        orgInfo = res.records[0];
-        sessionStorage.setItem(sfHost + "_orgInfo", JSON.stringify(orgInfo));
-      });
-    }
   }
   isMac() {
     return navigator.userAgentData?.platform.toLowerCase().indexOf("mac") > -1 || navigator.userAgent.toLowerCase().indexOf("mac") > -1;
@@ -697,7 +687,8 @@ class AllDataBox extends React.PureComponent {
           sobjectsLoading: false,
           sobjectsList: Array.from(entityMap.values())
         });
-        this.refs.showAllDataBoxSObject.refs.allDataSearch.getMatchesDelayed("");
+        // Only call getMatchesDelayed if the showAllDataBoxSObject component is rendered (i.e., user is on Objects tab)
+        this.refs.showAllDataBoxSObject?.refs?.allDataSearch?.getMatchesDelayed("");
       })
       .catch(e => {
         console.error(e);
@@ -1363,9 +1354,12 @@ class AllDataBoxOrg extends React.PureComponent {
     this.deleteApexLogs = this.deleteApexLogs.bind(this);
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     let {sfHost} = this.props;
     let orgInfo = JSON.parse(sessionStorage.getItem(sfHost + "_orgInfo"));
+    if (!orgInfo){
+      orgInfo = await setOrgInfo(sfHost);
+    }
     this.setInstanceStatus(orgInfo.InstanceName, sfHost);
   }
 
@@ -1470,15 +1464,15 @@ class AllDataBoxOrg extends React.PureComponent {
               h("tbody", {},
                 h("tr", {},
                   h("th", {}, h("a", {href: "https://" + sfHost + "/lightning/setup/CompanyProfileInfo/home", title: "Company Information", target: linkTarget, onClick: handleLightningLinkClick}, "Org Id:")),
-                  h("td", {}, orgInfo.Id.substring(0, 15))
+                  h("td", {}, orgInfo?.Id.substring(0, 15))
                 ),
                 h("tr", {},
-                  h("th", {}, h("a", {href: "https://status.salesforce.com/instances/" + orgInfo.InstanceName, title: "Instance status", target: linkTarget}, "Instance:")),
-                  h("td", {}, orgInfo.InstanceName)
+                  h("th", {}, h("a", {href: "https://status.salesforce.com/instances/" + orgInfo?.InstanceName, title: "Instance status", target: linkTarget}, "Instance:")),
+                  h("td", {}, orgInfo?.InstanceName)
                 ),
                 h("tr", {},
                   h("th", {}, "Type:"),
-                  h("td", {}, orgInfo.OrganizationType)
+                  h("td", {}, orgInfo?.OrganizationType)
                 ),
                 h("tr", {},
                   h("th", {}, "Status:"),
@@ -1497,7 +1491,7 @@ class AllDataBoxOrg extends React.PureComponent {
                   h("td", {}, this.getApiVersion(this.state.instanceStatus))
                 ),
                 h("tr", {},
-                  h("th", {}, h("a", {href: "https://status.salesforce.com/instances/" + orgInfo.InstanceName + "/maintenances", title: "Maintenance List", target: linkTarget}, "Maintenance:")),
+                  h("th", {}, h("a", {href: "https://status.salesforce.com/instances/" + orgInfo?.InstanceName + "/maintenances", title: "Maintenance List", target: linkTarget}, "Maintenance:")),
                   h("td", {}, this.getNextMajorRelease(this.state.instanceStatus?.Maintenances))
                 ),
               )

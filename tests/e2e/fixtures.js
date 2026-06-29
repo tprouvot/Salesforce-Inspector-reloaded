01867f13cdca1083fbe81b8ae8fc7b5bc68edfb1
landrybr <blan5907@gmail.com>
2026-06-29 19:14:00 -0400

    fix: Use chrome channel on CI

--- tests/e2e/fixtures.js
+++ tests/e2e/fixtures.js
@@ -9,7 +9,7 @@ export const test = base.extend({
     const pathToExtension = path.join(process.cwd(), "addon");
     const context = await chromium.launchPersistentContext("", {
       headless: true,
-      channel: "chromium",
+      channel: process.env.CI ? "chrome" : "chromium",
       args: [
         `--disable-extensions-except=${pathToExtension}`,
         `--load-extension=${pathToExtension}`,

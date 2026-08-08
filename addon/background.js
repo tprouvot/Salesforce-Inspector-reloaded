39be7c127db0217ace10b66c353165156e6f4c70
Prem Kumar <prem12321kumar@gmail.com>
2026-08-09 03:51:56 +0530

    Prevent command execution without sfHost
    Add a guard clause to check if sfHost is defined before processing keyboard commands. This prevents errors when commands are triggered without an active Salesforce host connection.

--- addon/background.js
+++ addon/background.js
@@ -64,6 +64,9 @@ chrome.action.onClicked.addListener(() => {
   });
 });
 chrome.commands?.onCommand.addListener((command) => {
+  if (!sfHost) {
+    return;
+  }
   if (command.startsWith("link-")){
     let link;
     switch (command){
